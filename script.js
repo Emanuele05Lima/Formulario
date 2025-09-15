623f0e4f65765018fbf813fae2a751f3a658adfd
Emanuele05Lima <krassowski.emanuele@escola.pr.gov.br>
2025-09-15 11:30:28 -0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
-
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
@@ -10,6 +9,7 @@ function mostraPalavrasChave() {
 
     campoResultado.textContent = palavrasChave.join(", ");
 }
+
 function mostraPalavrasChave() { 
   let palavras = texto.split(/\P{L}+/u);
 
